239bd0f70f9c499f7e9df67468eab89f52bc1c50
Bruno <Bruno@10.225.172.217>
2013-11-15 14:14:03 +0000

    kata5 terminada

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -5,8 +5,7 @@ public class Kata5 {
     public static void main(String[] args) {
         MailLoader loader = new MockMailLoader();
         HistogramBuilder<Mail> builder = new HistogramBuilder<>(loader.load());
-
-        ConsoleHistogramViewer<String> viewer = new ConsoleHistogramViewer<>();
+        ChartHistogramViewer<String> viewer = new ChartHistogramViewer<>();
         viewer.show(builder.build(new AtributeExtractor<Mail, String>() {
 
             @Override
